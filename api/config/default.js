d34fb3c1a09e6526f2bb22ddfd0d190002da9dc2
Donald Geddes <hi@donaldgeddes.ca>
2018-09-02 19:03:49 -0700

    Add comments for config props

--- api/config/default.js
+++ api/config/default.js
@@ -10,11 +10,22 @@ const { version } = require('../package.json')
 const knexConfig = require('../knexfile')[env]
 
 module.exports = {
+  // NODE_ENV
   env,
+
+  // Self-explanatory
   ip: ip.address(),
+
+  // Port express is listening on
   port: 4000,
+
+  // NPM version
   version,
+
+  // TMDb API key
   tmdbApiKey: process.env.TMDB_API_KEY,
+
+  // Database config (auto NODE_ENV-dependent)
   database: {
     ...knexConfig,
   },
